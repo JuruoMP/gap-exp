1f9ec92ed8c5adaf69eb3284c06989b536452c15
juruo <815732072@qq.com>
2021-10-08 20:14:28 +0800

    update

--- rat-sql-gap/seq2struct/utils/evaluation.py
+++ rat-sql-gap/seq2struct/utils/evaluation.py
@@ -6,6 +6,23 @@ import _jsonnet
 from seq2struct import datasets
 from seq2struct.utils import registry
 
+
+def post_process(infer_data, data_path):
+    dialog_lens = []
+    data = json.load(open(data_path, 'r', encoding='utf-8'))
+    for dialog in data:
+        interactions = dialog['interaction']
+        dialog_lens.append(len(interactions))
+
+    with open('predicted_sql.txt', 'w', encoding='utf-8') as fw:
+        for sql in infer_data:
+            fw.write(sql + '\n')
+        if dialog_lens[0] == 0:
+            del dialog_lens[0]
+            fw.write('\n')
+        else:
+            dialog_lens[0] -= 1
+
 def compute_metrics(config_path, config_args, section, inferred_path,logdir=None):
     if config_args:
         config = json.loads(_jsonnet.evaluate_file(config_path, tla_codes={'args': config_args}))
@@ -26,11 +43,12 @@ def compute_metrics(config_path, config_args, section, inferred_path,logdir=None
         raise Exception('Not enough inferred: {} vs {}'.format(len(inferred_lines),
           len(data)))
 
-
+    infer_data = []
     for line in inferred_lines:
         infer_results = json.loads(line)
         if infer_results['beams']:
             inferred_code = infer_results['beams'][0]['inferred_code']
+            infer_data.append(inferred_code)
         else:
             inferred_code = None
         if 'index' in infer_results:
